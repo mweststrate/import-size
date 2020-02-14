50d04905560f8997f4c1b06baef62a587079f05c
Michel Weststrate <mweststrate@fb.com>
2020-02-14 16:41:11 +0000

    first working version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const arg = require("arg");
 
-const {importCost, cleanup, JAVASCRIPT} = require("import-cost");
+const { importCost, cleanup, JAVASCRIPT } = require("import-cost");
 
 async function analyze(dir, library, methods) {
   let resolve, reject;
@@ -9,26 +9,34 @@ async function analyze(dir, library, methods) {
   const p = new Promise((_resolve, _reject) => {
     resolve = _resolve;
     reject = _reject;
-  })
-  const emitter = importCost(path.join(dir, "import-size.js", `import {${methods.join(",")}} from '${library}'`), JAVASCRIPT /* or TYPESCRIPT */);
-    emitter.on('error', (e)=> {
-      stop()
-      reject(e)
-    });
-    emitter.on('start', packages => {
-      console.log('start', JSON.stringify(packages, null, 2));
-    });
-    emitter.on('calculated', package => {
-      console.log('calculated', JSON.stringify(package, null, 2));
-    });
-    emitter.on('done', packages => {
-      console.log('done', JSON.stringify(packages, null, 2));
-      stop();
-      resolve();
-    });
-    
+  });
+
+  const target = path.join(dir, "import-size.js");
+  const emitter = importCost(
+    target,
+    // TODO: what about default, *, nothing?
+    `import {${methods.join(",")}} from '${library}'`,
+    JAVASCRIPT
+  );
+
+  emitter.on("error", e => {
+    stop();
+    reject(e);
+  });
+  emitter.on("done", packages => {
+    stop();
+    if (packages.length === 0) {
+      return reject("No packages found");
+    }
+    if (packages.length > 1) {
+      console.warn("Multiple packages found");
+    }
+    console.log(packages[0].gzip);
+    resolve();
+  });
+
   function stop() {
-    emitter.removeAllListeners(); 
+    emitter.removeAllListeners();
     cleanup();
   }
 
@@ -37,31 +45,38 @@ async function analyze(dir, library, methods) {
 
 function main() {
   const args = arg({
-    '--help': Boolean,
-    '--version': Boolean
-  })
+    "--help": Boolean,
+    "--version": Boolean
+  });
 
   if (args["--version"]) {
-    console.log(require('./package.json').version);
-    process.exit(0)
+    console.log(require("./package.json").version);
+    process.exit(0);
   }
   if (args["--help"]) {
-    console.log("Usage: import-size [library] [...methods]\nExample: import-size mobx autorun observable");
+    console.log(
+      "Usage: import-size [library] [...methods]\nExample: import-size mobx autorun observable"
+    );
     process.exit(0);
   }
 
   if (args._.length < 2) {
-    console.error("requires at least two arguments")
-    process.exit(1)
+    console.error("requires at least two arguments");
+    process.exit(1);
   }
 
-  const[library, ...methods] = args._;
-  analyze(process.cwd(), library, methods).then(() => {
-    process.exit(0)
-  }, e => {
-    console.error(e);
-    process.exit(1)
-  });
+  const [library, ...methods] = args._;
+  analyze(process.cwd(), library, methods).then(
+    () => {
+      process.exit(0);
+    },
+    e => {
+      console.error(e);
+      process.exit(1);
+    }
+  );
 }
 
 main();
+
+module.exports = { analyze };
